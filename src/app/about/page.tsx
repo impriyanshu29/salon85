21edd3a5ed92e9f460ade8124d015507dd1cd80f
Priyanshu Tiwari <impriyanshu2901@gmail.com>
2024-09-05 09:14:25 +0530

    Added Images

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -42,13 +42,13 @@ export default function AboutPageOne() {
       <div className="mx-auto my-3 py-8 max-w-7xl px-4 sm:px-6 md:my-24 lg:my-32 lg:px-8">
 
 
-        <div className="mx-auto pt-12 lg:pt-4 justify-center max-w-xl pb-9 text-center">
+        <div className="mx-auto pt-12  lg:pt-4 justify-center max-w-xl pb-9 text-center">
           <div className="mx-auto inline-flex rounded-full bg-iconBg dark:bg-black px-4 py-1.5">
             <p className="text-xs font-semibold uppercase tracking-widest text-background">
               About Us
             </p>
           </div>
-          <h2 className="mt-6 text-3xl font-bold leading-tight font-headingFont text-iconBg justify-center text-center mx-auto dark:text-gray-200 sm:text-4xl lg:text-5xl">
+          <h2 className="mt-6 text-3xl hidden md:flex font-bold leading-tight font-headingFont text-iconBg justify-center text-center mx-auto dark:text-gray-200 sm:text-4xl lg:text-5xl">
             Discover more about Salon85
           </h2>
         </div>
@@ -56,7 +56,7 @@ export default function AboutPageOne() {
         {/* About page Intro */}
         <div className="mx-auto max-w-6xl lg:grid lg:grid-cols-12 lg:gap-x-8 lg:px-8">
           <div className="flex flex-col justify-center px-2 py-2 md:py-12 lg:col-span-8 lg:gap-x-6 lg:px-6 lg:py-10 xl:col-span-7">
-            <div className="mt-8 flex max-w-max items-center justify-center lg:justify-normal mx-auto lg:mx-0 space-x-2 rounded-full bg-header dark:bg-black  p-2">
+            <div className=" hidden md:flex mt-8 max-w-max items-center justify-center lg:justify-normal mx-auto lg:mx-0 space-x-2 rounded-full bg-header dark:bg-black  p-2">
               <div className="rounded-full bg-iconBg dark:bg-zinc-900/70 p-1 px-2">
                 <p className="text-sm text-background font-medium">Know</p>
               </div>
@@ -64,7 +64,7 @@ export default function AboutPageOne() {
             </div>
 
 
-            <h1 className="mt-8 text-2xl font-bold font-headingFont leading-normal    text-iconBg lg:text-left text-center md:text-3xl lg:text-4xl dark:text-gray-200">
+            <h1 className="-mt-4 md:mt-8 text-2xl font-bold font-headingFont leading-normal    text-iconBg lg:text-left text-center md:text-3xl lg:text-4xl dark:text-gray-200">
               Step into Luxury at Salon85, Park Street's Leading VIP Salon
             </h1>
             <p className="mt-4 text-center lg:text-left text-base md:text-lg  font-subheadingFont text-header dark:text-gray-300">
